329443108923fee652db463f6a71b2eb6c76f1b2
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-04-29 10:57:02 +0200

    Haciendo pruebas

--- CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Bus.java
+++ CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Bus.java
@@ -1,33 +1,25 @@
 package com.salesianostriana.dam.castillacanoalvaroproyecto1.modelo;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor @AllArgsConstructor
 @Entity
-public class Evento {
-	@Id @GeneratedValue
+public class Bus {
+
+	@Id
+	@GeneratedValue
 	private Long id;
 	
-	private String nombre;
+	private double kmRecorridos;
 	
-	private LocalDate fecha;
+	private boolean contratado;
 	
-	private LocalDateTime hora;
+	private double descuento;
 	
-	private double duracion;
-	
-	private String lugar;
-	
-	private int musicosNecesarios;
-	
-	private double dineroPagado;
-	
-	//Completar atributos están incompletos.
 }
