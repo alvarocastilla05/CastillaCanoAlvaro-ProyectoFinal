7fd0f5b2032bb4517a9f36de2d9c66f89025c786
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-05-02 20:21:40 +0200

    Retoques

--- CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Concierto.java
+++ CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Concierto.java
@@ -1,8 +1,19 @@
 package com.salesianostriana.dam.castillacanoalvaroproyecto1.modelo;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Concierto extends Evento{
 
-
-public class Concierto {
-
+	private int aforo;
+	
+	private boolean gratuito;
+	
+	private double precioHoraExtra;
 }
--- CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Procesion.java
+++ CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/modelo/Procesion.java
@@ -1,19 +1,24 @@
 package com.salesianostriana.dam.castillacanoalvaroproyecto1.modelo;
 
 import jakarta.persistence.Entity;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter @Setter
-@EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
+
+
+@Data
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @Entity
 public class Procesion extends Evento {
 
+	private String hermandad, titular;
+	
+	private double precioHorasExtras;
+
+	
+	
 	
 	
 	
