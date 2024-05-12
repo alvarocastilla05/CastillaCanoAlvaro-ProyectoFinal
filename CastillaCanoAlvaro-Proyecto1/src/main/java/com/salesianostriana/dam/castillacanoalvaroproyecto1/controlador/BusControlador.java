48612449a9bc51a9b520d3eeabf23562be4e5443
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-05-12 14:33:32 +0200

    Cambios

--- CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/controlador/BusControlador.java
+++ CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/controlador/BusControlador.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.castillacanoalvaroproyecto1.modelo.Bus;
@@ -22,5 +24,11 @@ public class BusControlador {
 		model.addAttribute("bus", bus);
 		
 		return "admin/contratarBus";
+	}
+	
+	@PostMapping("/contratar/submit")
+	public String guardarBus(@ModelAttribute("bus") Bus bus) {
+		busServicio.save(bus);
+		return "admin/tablaBuses";
 	}
 }
