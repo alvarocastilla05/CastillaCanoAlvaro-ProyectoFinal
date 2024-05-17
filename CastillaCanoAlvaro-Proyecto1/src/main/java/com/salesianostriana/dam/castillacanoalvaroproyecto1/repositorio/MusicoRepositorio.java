bbee3264e006eb1dd43f8d961470963f89abe636
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-05-17 20:02:19 +0200

    Buscar por palabras clave en el listado de componentes, e importación de 100 musico

--- CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/repositorio/MusicoRepositorio.java
+++ CastillaCanoAlvaro-Proyecto1/src/main/java/com/salesianostriana/dam/castillacanoalvaroproyecto1/repositorio/MusicoRepositorio.java
@@ -1,12 +1,22 @@
 package com.salesianostriana.dam.castillacanoalvaroproyecto1.repositorio;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.salesianostriana.dam.castillacanoalvaroproyecto1.modelo.Musico;
 
 public interface MusicoRepositorio extends JpaRepository<Musico, Long>{
 
 	Optional<Musico> findFirstByUsername(String username);
+	
+	@Query("""
+			SELECT m FROM Musico m 
+			WHERE CONCAT(m.id, m.nombre, m.apellidos, 
+				m.username, m.tipoInstrumento, m.fechaAlta)
+				LIKE %?1%
+			""")
+	public List<Musico> findAllByPalabraClave(String palabraClave);
 }
